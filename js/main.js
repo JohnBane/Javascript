fa0e803bb63e1143e9ea72ed0841a7a06a3f798a
John <shadycrown312@gmail.com>
2020-12-09 00:30:04 +0600

    If else, switch, case

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+"use strict";
 
-console.log(`Вы просмотрели ${numberOfFilms} фильмов`);
+let num = 49;
 
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-
-};
-
-let a = prompt("Один из последних просмотренных фильмов?",""),
-     b = prompt("На сколько оцените его?",""),
-     c = prompt("Один из последних просмотренных фильмов?",""),
-     d = prompt("На сколько оцените его?","");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
-
+(num === 50) ? console.log("true") : console.log("error");
