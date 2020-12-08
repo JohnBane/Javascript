04d545823e74b2e921fd45540eb9c24bde19f3ca
John <shadycrown312@gmail.com>
2020-12-09 02:20:21 +0600

    Practic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
-"use strict";
 
-let num = 50;
 
-// while (num <= 49){
-//     console.log(num);
-//     num++;
-// }
-// do {
-//     console.log(num);
-//     num++;
-// } while(num<10);
-for(let i = 0; i <= 10; i++){
-   if( i === 7){
-       continue;
-   }
-    console.log(i);
+
+
+// let a = prompt("Один из последних просмотренных фильмов?",""),
+//      b = prompt("На сколько оцените его?",""),
+//      c = prompt("Один из последних просмотренных фильмов?",""),
+//      d = prompt("На сколько оцените его?","");
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+
+console.log(`Вы просмотрели ${numberOfFilms} фильмов`);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+let lastFilms, ocenkaFilms;
+for(let i = 0; i < 2; i++){
+    lastFilms = prompt("Один из последних просмотренных фильмов?","");
+    ocenkaFilms = prompt("На сколько оцените его","");
+    personalMovieDB.movies[lastFilms] = ocenkaFilms;
+    console.log(personalMovieDB);
 }
+
