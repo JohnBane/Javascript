fdd31c0856dda71d725c59812e3079078847d8d9
John <shadycrown312@gmail.com>
2020-12-09 16:00:30 +0600

    function declaration expression =>

--- js/main.js
+++ js/main.js
@@ -1,53 +1,30 @@
+"use strict";
 
+function getName(name){
+    console.log(name);
+}
 
+getName("Adilet");
 
+function Calc( a, b){
+    return (a + b);
+}
+console.log(Calc(3,3));
+console.log(Calc(4,4));
+console.log(Calc(10,10));
 
-// let a = prompt("Один из последних просмотренных фильмов?",""),
-//      b = prompt("На сколько оцените его?",""),
-//      c = prompt("Один из последних просмотренных фильмов?",""),
-//      d = prompt("На сколько оцените его?","");
-// 
+function ret(num){
+    num = 50;
+    return num;
+}
+let anotherNum = ret();
+console.log(anotherNum);
 
-// console.log(personalMovieDB);
-
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-console.log(`Вы просмотрели ${numberOfFilms} фильмов`);
-
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-
+let num = function(a,b){
+    return a + b;
 };
+console.log(num(2,3));
 
+const qwe = (a,b) => a + b;
 
-for(let i = 0; i < 2; i++){
-    const nameFilms = prompt("Один из просмотренных фильмов?",""),
-        raitingFilms = prompt("Оценка фильма?","");
-   
-   
-
-    if(nameFilms != null && raitingFilms != null && nameFilms != `` && raitingFilms != `` && nameFilms.length < 50){
-        personalMovieDB.movies[nameFilms] = raitingFilms;
-        console.log("done");
-    } else {
-        console.log("Error");
-        i--;
-    }
-}
-console.log(personalMovieDB);
-if(personalMovieDB.count < 10){
-    console.log("Просмотрено довольно мало фильмов");
-} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    console.log("Вы классический зритель");
-} else if(personalMovieDB.count >= 30){
-    console.log("Вы киноман");
-} else {
-    console.log("Error");
-}
-
-
-
+console.log(qwe(2,3));
