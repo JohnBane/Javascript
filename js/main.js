11da56e71a46402827bd0117e1ac22ddfcd6fba8
John <shadycrown312@gmail.com>
2021-01-05 22:12:32 +0600

    AddEventListenner preventDefault once: true

--- js/main.js
+++ js/main.js
@@ -1,31 +1,56 @@
+// // "use strict";
+
+// // let btns = document.querySelectorAll("button");
+// // let circles = document.querySelectorAll(".circle");
+// // let hearts = document.querySelector("hearts");
+// // let wrapper = document.querySelector(".wrapper");
+
+// // btns[0].style.backgroundColor = "red";
+
+// // // for (let i = 0; i < circles.length; i++){
+// // //     circles[i].style.backgroundColor = "blue";
+// // // }
+// // // hearts.style.classList = "border: 1px solid black; border-radius: 2px;";
+
+// // circles.forEach(item => {   
+// //     item.style.backgroundColor = "red";
+// // });
+
+// // const div = document.createElement("div");
+// // const div2 = document.createElement("div");
+// // div.classList.add("yellow");
+// // div2.classList.add("yellow");
+
+// // document.body.append(div);
+// // div.innerHTML = "<h1>Hello World!!!</h1> <p>This is paragraph</p>";
+
+// // let oneButton = document.createElement("button");
+// // oneButton.innerHTML = "6";
+// // btns[4].after(oneButton);
 "use strict";
 
-let btns = document.querySelectorAll("button");
-let circles = document.querySelectorAll(".circle");
-let hearts = document.querySelector("hearts");
-let wrapper = document.querySelector(".wrapper");
+const btns = document.querySelectorAll("button"),
+      overlay = document.querySelector(".overlay"),
+      link = document.querySelector("a");
+// let i = 0;
+const deleteValue = (e) => {
+    console.log(e.target);
+    console.log(e.type);
+    // i++;
+    // if( i == 1){
+    //     btn.removeEventListener("click", deleteValue);
+    // }
+};
+// link.addEventListener("click", function(event){
+//     event.preventDefault();
 
-btns[0].style.backgroundColor = "red";
+//     console.log(event.target);
+// });
 
-// for (let i = 0; i < circles.length; i++){
-//     circles[i].style.backgroundColor = "blue";
-// }
-// hearts.style.classList = "border: 1px solid black; border-radius: 2px;";
-
-circles.forEach(item => {   
-    item.style.backgroundColor = "red";
+// btn.addEventListener("click", deleteValue);
+// overlay.addEventListener("click", deleteValue);
+btns.forEach(btn => {
+    btn.addEventListener("click", deleteValue);
 });
 
-const div = document.createElement("div");
-const div2 = document.createElement("div");
-div.classList.add("black");
-div2.classList.add("yellow");
 
-document.body.prepend(div);
-
-circles[0].before(div);
-circles[2].after(div2);
-
-circles[0].replaceWith(btns[0]);
-
-btns[2].replaceWith(circles[1]);
